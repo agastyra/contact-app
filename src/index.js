aa4ea975efd57468e2c89599ab2a0d296ba0f211
Rangga Agastya <rangga.agastya711@gmail.com>
2024-01-07 21:46:22 +0700

    create new routing for homepage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const path = require("path");
 const dotenv = require("dotenv");
+const { appInit, loadContacts } = require("./contact");
 
 const app = express();
 dotenv.config();
@@ -8,11 +10,26 @@ const PORT = process.env.PORT;
 
 app.set("view engine", "ejs");
 app.use(expressLayouts);
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static("public"));
+
+app.use((req, res, next) => {
+  appInit();
+  next();
+});
 
 app.get("/", (req, res) => {
+  const contacts = loadContacts();
+
   res.render("home", {
     layout: "layouts/app",
+    contacts,
   });
+});
+
+app.use((req, res) => {
+  res.status(404);
+  res.send(`Url '${req.url}' is not found! Please type correct url!`);
 });
 
 app.listen(PORT, () => {
